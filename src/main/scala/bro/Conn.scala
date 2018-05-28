64b712f5989253cd74c332746124bd8152c6d3e1
Victor Garcia <vikgarcia.dev@gmail.com>
2018-05-28 18:46:50 +0700

    Cambios

--- src/main/scala/bro/Conn.scala
+++ src/main/scala/bro/Conn.scala
@@ -19,7 +19,7 @@ object Conn {
                        )
 
   case class Simple (
-                  timestamp: Option[Long],
+                  timestamp: String,
                   uid: String,
                   source_ip: String,
                   source_port: Option[Int],
