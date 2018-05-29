90a9affcb4bcc25980379917191abfdf5bb00d54
Victor Garcia <vikgarcia.dev@gmail.com>
2018-05-29 20:00:46 +0700

    Cambios

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,7 +15,7 @@ object Main {
     startNewPipeline(kafkaTopicConn, "Conn")
     */
 
-    val streamingDataFrame = spark.readStream.schema(Conn.schemaBaseCSV).csv("/opt/data/toyota-data/logs-conn/conn.csv")
+    val streamingDataFrame = spark.readStream.schema(Conn.schemaBaseCSV).csv("/opt/data/csv/")
     KafkaSource.write(streamingDataFrame, "conn-csv")
 
 
