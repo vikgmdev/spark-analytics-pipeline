93fae4a489b55cc4e64bdaaec4a4998ea44feff8
Victor Garcia <vikgarcia.dev@gmail.com>
2018-05-30 04:48:29 +0700

    Cambios

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -26,7 +26,7 @@ object Main {
       .toDF()
       .writeStream
       .format(s"base.SinkProvider")
-      .option("pipeline", s"production.pipelines.Pipeline$whichProvider")
+      .option("pipeline", s"Pipeline$whichProvider")
       .outputMode(OutputMode.Update())
       .queryName(s"KafkaStreamToPipeline$whichProvider")
       .start()
