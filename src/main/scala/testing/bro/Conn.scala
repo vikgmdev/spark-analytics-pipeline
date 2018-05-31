d150693456c400a5fb7d5243a33e53bf7db8be32
Victor Garcia <vikgarcia.dev@gmail.com>
2018-06-01 02:18:46 +0700

    Cambios

--- src/main/scala/testing/bro/Conn.scala
+++ src/main/scala/testing/bro/Conn.scala
@@ -19,41 +19,41 @@ object Conn extends LogBase {
                   proto: String,
                   service: String,
                   direction: String,
-                  duration: Option[Double],
-                  orig_bytes: Option[Double],
-                  resp_bytes: Option[Double],
+                  duration: Option[Int],
+                  orig_bytes: Option[Int],
+                  resp_bytes: Option[Int],
                   conn_state: String,
                   local_orig: Option[Boolean],
                   local_resp: Option[Boolean],
-                  missed_bytes: Option[Double],
+                  missed_bytes: Option[Int],
                   history: String,
-                  orig_pkts: Option[Double],
-                  orig_ip_bytes: Option[Double],
-                  resp_pkts: Option[Double],
-                  resp_ip_bytes: Option[Double],
-                  tunnel_parents: Vector[String]
+                  orig_pkts: Option[Int],
+                  orig_ip_bytes: Option[Int],
+                  resp_pkts: Option[Int],
+                  resp_ip_bytes: Option[Int],
+                  tunnel_parents: Option[Vector[String]]
                  ) extends Serializable
 
   val schemaBase: StructType = new StructType()
     .add("ts", StringType)
     .add("uid", StringType)
     .add("id.orig_h", StringType)
-    .add("id.orig_p", StringType)
+    .add("id.orig_p", IntegerType)
     .add("id.resp_h", StringType)
-    .add("id.resp_p", StringType)
+    .add("id.resp_p", IntegerType)
     .add("proto", StringType)
     .add("service", StringType)
-    .add("duration", StringType)
-    .add("orig_bytes", StringType)
-    .add("resp_bytes", StringType)
+    .add("duration", IntegerType)
+    .add("orig_bytes", IntegerType)
+    .add("resp_bytes", IntegerType)
     .add("conn_state", StringType)
-    .add("local_orig", StringType)
-    .add("local_resp", StringType)
-    .add("missed_bytes", StringType)
+    .add("local_orig", BooleanType)
+    .add("local_resp", BooleanType)
+    .add("missed_bytes", IntegerType)
     .add("history", StringType)
-    .add("orig_pkts", StringType)
-    .add("orig_ip_bytes", StringType)
-    .add("resp_pkts", StringType)
-    .add("resp_ip_bytes", StringType)
+    .add("orig_pkts", IntegerType)
+    .add("orig_ip_bytes", IntegerType)
+    .add("resp_pkts", IntegerType)
+    .add("resp_ip_bytes", IntegerType)
     .add("tunnel_parents", ArrayType(StringType))
 }
--- src/main/scala/testing/pipelines/PipelineConn.scala
+++ src/main/scala/testing/pipelines/PipelineConn.scala
@@ -1,8 +1,8 @@
 package testing.pipelines
 
 import base.SinkBase
-import org.apache.spark.sql.functions.{col, from_json}
-import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Dataset}
 import testing.bro.Conn
 import testing.enrichment.ConnEnrichment._
@@ -32,11 +32,27 @@ class PipelineConn() extends SinkBase {
     df.withColumn("data",
       from_json($"value".cast(StringType), Conn.schemaBase))
       .select("data.*")
+      // Change column's to the righ type
+      .withColumn("id.orig_p", $"id.orig_p".cast(IntegerType))
+      .withColumn("id.resp_p", $"id.resp_p".cast(IntegerType))
+      .withColumn("duration", $"duration".cast(IntegerType))
+      .withColumn("orig_bytes", $"orig_bytes".cast(IntegerType))
+      .withColumn("resp_bytes", $"resp_bytes".cast(IntegerType))
+      .withColumn("local_orig", $"local_orig".cast(BooleanType))
+      .withColumn("local_resp", $"local_resp".cast(BooleanType))
+      .withColumn("missed_bytes", $"missed_bytes".cast(IntegerType))
+      .withColumn("orig_pkts", $"orig_pkts".cast(IntegerType))
+      .withColumn("orig_ip_bytes", $"orig_ip_bytes".cast(IntegerType))
+      .withColumn("resp_pkts", $"resp_pkts".cast(IntegerType))
+      .withColumn("resp_ip_bytes", $"resp_ip_bytes".cast(ArrayType(StringType)))
+
+      // Rename column normalization
       .withColumnRenamed("ts", "timestamp")
       .withColumnRenamed("id.orig_h", "source_ip")
       .withColumnRenamed("id.orig_p", "source_port")
       .withColumnRenamed("id.resp_h", "dest_ip")
       .withColumnRenamed("id.resp_p", "dest_port")
+
       .withColumn("direction", withDirection(col("local_orig"), col("local_resp")))
       // .withColumn("pcr", withPCR(col("direction"), col("orig_bytes"), col("resp_bytes")))
       .as[Conn.Simple]
