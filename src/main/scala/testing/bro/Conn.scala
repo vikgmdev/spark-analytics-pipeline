85362f0fcea636f25a4cec7cf2e41d9d99157201
Victor Garcia <vikgarcia.dev@gmail.com>
2018-06-01 03:09:55 +0700

    Cambios

--- src/main/scala/testing/bro/Conn.scala
+++ src/main/scala/testing/bro/Conn.scala
@@ -31,8 +31,8 @@ object Conn extends LogBase {
                   orig_ip_bytes: Option[Double],
                   resp_pkts: Option[Double],
                   resp_ip_bytes: Option[Double],
-                  tunnel_parents: Option[Vector[String]]
-                  // pcr: Option[Double]
+                  tunnel_parents: Option[Vector[String]],
+                  pcr: Option[Double]
                  ) extends Serializable
 
   val schemaBase: StructType = new StructType()
