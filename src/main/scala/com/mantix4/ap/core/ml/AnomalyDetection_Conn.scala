405e95ae75e00bdf4cd13e51dc192e4c81479c42
Victor Garcia <vikgarcia.dev@gmail.com>
2018-09-07 00:03:32 +0800

    Added new logs objects

--- src/main/scala/com/mantix4/ap/core/ml/AnomalyDetection_Conn.scala
+++ src/main/scala/com/mantix4/ap/core/ml/AnomalyDetection_Conn.scala
@@ -20,29 +20,19 @@ object AnomalyDetection_Conn {
     val categoricalColumns = Array("proto", "direction")
     val numericCols = Array("pcr")
 
-    var assemblerInputs: Array[String] = Array()
-    var stages : Array[PipelineStage] = new Array[PipelineStage](6)
 
-    for (categoricalCol <- categoricalColumns) {
-      val stringIndexer = new StringIndexer()
-        .setInputCol(categoricalCol)
-        .setOutputCol(categoricalCol + "Index")
-      stages = stages :+ stringIndexer
+    // StringIndexer
+    val stringIndexer_1 = new StringIndexer().setInputCol("proto").setOutputCol("proto" + "Index")
+    val stringIndexer_2 = new StringIndexer().setInputCol("direction").setOutputCol("direction" + "Index")
 
-      val encoder = new OneHotEncoder()
-        .setInputCol(categoricalCol + "Index")
-        .setOutputCol(categoricalCol + "classVec")
-      stages = stages :+ encoder
-    }
+    // OneHotEncoder
+    val encoder_1 = new OneHotEncoder().setInputCol("proto" + "Index").setOutputCol("proto" + "classVec")
+    val encoder_2 = new OneHotEncoder().setInputCol("direction" + "Index").setOutputCol("direction" + "classVec")
 
-    for (categoricalCol <- categoricalColumns) {
-      assemblerInputs = numericCols :+ categoricalCol + "classVec"
-    }
-
+    var assemblerInputs = Array("proto" + "classVec", "direction" + "classVec", "pcr")
     val assembler = new VectorAssembler()
       .setInputCols(assemblerInputs)
       .setOutputCol("features")
-    stages = stages :+ assembler
 
     // Train/fit and Predict anomalous instances
     // using the Isolation Forest model
@@ -54,7 +44,7 @@ object AnomalyDetection_Conn {
       .setMaxDepth(100)
       .setSeed(123456L)
 
-    stages = stages :+ iForest
+    val stages = Array(stringIndexer_1, stringIndexer_2, encoder_1, encoder_2, assembler, iForest)
 
     val pipeline = new Pipeline().setStages(stages)
     val pipelineModel = pipeline.fit(dataset)
