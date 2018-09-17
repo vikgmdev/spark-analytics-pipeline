636b99f6f0b17a90d2483365b26da13a28310b84
Victor Garcia <vikgarcia.dev@gmail.com>
2018-09-17 09:09:52 +0800

    Added new logs objects

--- src/main/scala/com/mantix4/ap/core/enrichments/IpLookupEnricher.scala
+++ src/main/scala/com/mantix4/ap/core/enrichments/IpLookupEnricher.scala
@@ -15,7 +15,34 @@ object IpLookupEnricher {
   implicit class DataFrameTransforms(df: DataFrame) {
     def saveToCassandra(keyspaceName: String): DataFrame = {
       println(keyspaceName)
-      df.rdd.saveToCassandra(keyspaceName, "conn")
+      df.rdd.saveToCassandra(keyspaceName, "conn", SomeColumns("timestamp",
+        "uid",
+        "source_ip",
+        "source_port",
+        "dest_ip",
+        "dest_port",
+        "proto",
+        "service",
+        "duration",
+        "orig_bytes",
+        "resp_bytes",
+        "conn_state",
+        "local_orig",
+        "local_resp",
+        "missed_bytes",
+        "history",
+        "orig_pkts",
+        "orig_ip_bytes",
+        "resp_pkts",
+        "resp_ip_bytes",
+        "tunnel_parents",
+        "orig_l2_addr",
+        "resp_l2_addr",
+        "vlan",
+        "inner_vlan",
+        "direction",
+        "pcr",
+        "sensor"))
       df
     }
   }
