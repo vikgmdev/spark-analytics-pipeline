1e6b7bac5647746eb9e1a98f50e32cd5125dfe36
Victor Garcia <vikgarcia.dev@gmail.com>
2018-09-19 20:25:36 +0800

    Added new logs objects

--- src/main/scala/com/mantix4/ap/core/pipelines/PipelineConn.scala
+++ src/main/scala/com/mantix4/ap/core/pipelines/PipelineConn.scala
@@ -38,6 +38,8 @@ class PipelineConn() extends Pipeline[Conn.Conn](Conn.schemaBase) {
   override def customParsing(df: DataFrame): DataFrame = {
     df
         .drop("tunnel_parents")
+        .drop("vlan")
+        .drop("inner_vlan")
       //.withColumn("date", from_unixtime($"timestamp"))
       // .withColumn("tunnel_parents", split(col("tunnel_parents"), ","))
 
