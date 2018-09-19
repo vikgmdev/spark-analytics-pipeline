7bbe85669d9e04e0358a1562b13f9356d13d2a4c
Victor Garcia <vikgarcia.dev@gmail.com>
2018-09-19 20:41:40 +0800

    Added new logs objects

--- src/main/scala/com/mantix4/ap/core/enrichments/IpLookupEnricher.scala
+++ src/main/scala/com/mantix4/ap/core/enrichments/IpLookupEnricher.scala
@@ -17,7 +17,34 @@ object IpLookupEnricher {
     def saveToCassandra(keyspaceName: String): DataFrame = {
       println(keyspaceName)
       implicit val rowWriter: SqlRowWriter.Factory.type = SqlRowWriter.Factory
-      df.rdd.saveToCassandra(keyspaceName, "conn")
+      df.rdd.saveToCassandra(keyspaceName, "conn", SomeColumns("timestamp",
+        "uid",
+        "source_ip",
+        "source_port",
+        "dest_ip",
+        "dest_port",
+        "proto",
+        "service",
+        "duration",
+        "orig_bytes",
+        "resp_bytes",
+        "conn_state",
+        "local_orig",
+        "local_resp",
+        "missed_bytes",
+        "history",
+        "orig_pkts",
+        "orig_ip_bytes",
+        "resp_pkts",
+        "resp_ip_bytes",
+        "tunnel_parents",
+        "orig_l2_addr",
+        "resp_l2_addr",
+        "vlan",
+        "inner_vlan",
+        "sensor",
+        "direction",
+        "pcr"))
       df
     }
   }
@@ -48,9 +75,9 @@ object IpLookupEnricher {
     "resp_l2_addr",
     "vlan",
     "inner_vlan",
+    "sensor",
     "direction",
-    "pcr",
-    "sensor")
+    "pcr")
   */
 
 
