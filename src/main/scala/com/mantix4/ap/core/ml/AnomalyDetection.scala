08ddeef73a656a229eea5313da85a4a3ce266479
Victor Garcia <vikgarcia.dev@gmail.com>
2018-09-22 21:31:19 +0800

    Added new logs objects

--- src/main/scala/com/mantix4/ap/core/ml/AnomalyDetection.scala
+++ src/main/scala/com/mantix4/ap/core/ml/AnomalyDetection.scala
@@ -1,13 +1,12 @@
 package com.mantix4.ap.core.ml
 
 import com.mantix4.ap.abstracts.spark.SparkHelper
-import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.ml.{Pipeline, PipelineStage, _}
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.feature.{OneHotEncoder, PCA, StringIndexer, VectorAssembler}
 import org.apache.spark.ml.iforest.IForest
 import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.functions._
-import org.apache.spark.ml._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -23,6 +22,43 @@ object AnomalyDetection {
     // Initialize Array of Pipeline's Stages
     var stages = ArrayBuffer[PipelineStage]()
 
+    // Add Pipeline stages to normalize categorical and numeric features
+    stages = setupFeaturesNormalizerPipeline(stages, categoricalColumns, numericCols)
+
+    // Add Isolation Forest prediction to the pipeline
+    stages = setupIsolationForestPipeline(stages)
+
+    // Configure an ML pipeline, which consists of four stages: stringIndexer, encoder, assembler and iForest.
+    val pipeline = new Pipeline()
+      .setStages(stages.toArray)
+
+    // Train/fit and Predict anomalous instances
+    val pipelineModel = pipeline.fit(dataset)
+
+    // Now create a new dataframe using the prediction from our classifier
+    val predictions_dataset = pipelineModel.transform(dataset)
+
+    // Select only "uid" that is the log's id and the features column containing the Vector predictions
+    // Create new dataframe to not override the original dataset
+    var featured_dataset = predictions_dataset.select("uid", "features")
+
+    var dataframe_with_clusters = predictClustering(featured_dataset)
+
+    val pca_dataframe = predictPCA(dataframe_with_clusters)
+
+    // To end join the dataframe with all the anomalous instances
+    // needed for our detection with the original dataset containing the full data log
+    val outlier_dataset = predictions_dataset.join(pca_dataframe, "uid")
+
+    // Log end time of the full Anomaly Detection prediction, just for debug
+    var endTime = System.currentTimeMillis()
+    println(s"Anomaly Detection time: ${(endTime - startTime) / 1000} seconds.")
+
+    // Return original dataset with the new outliers columns "x", "y" and "cluster"
+    outlier_dataset
+  }
+
+  def setupFeaturesNormalizerPipeline(stages: ArrayBuffer[PipelineStage], categoricalColumns: Array[String], numericCols: Array[String]): ArrayBuffer[PipelineStage] = {
     // Map Categorical columns features to numeric values
     for (categoricalCol <- categoricalColumns) {
 
@@ -60,6 +96,11 @@ object AnomalyDetection {
     // Add Stage to the Array
     stages += assembler
 
+    // return the stages array
+    stages
+  }
+
+  def setupIsolationForestPipeline(stages: ArrayBuffer[PipelineStage]): ArrayBuffer[PipelineStage] = {
     // Train/fit and Predict anomalous instances
     // using the Isolation Forest model
     val iForest = new IForest()
@@ -73,34 +114,20 @@ object AnomalyDetection {
     // Add Stage to the Array
     stages += iForest
 
-    // Configure an ML pipeline, which consists of four stages: stringIndexer, encoder, assembler and iForest.
-    val pipeline = new Pipeline()
-      .setStages(stages.toArray)
+    // return the stages array
+    stages
+  }
 
-    // Train/fit and Predict anomalous instances using the Isolation Forest model
-    val pipelineModel = pipeline.fit(dataset)
-    println("Fit model:")
-    println(pipelineModel.stages)
-    println(pipelineModel.uid)
-    dataset.show(false)
-
-    // Now create a new dataframe using the prediction from our classifier
-    val predictions_dataset = pipelineModel.transform(dataset)
-    println("Transform model:")
-    predictions_dataset.show(false)
-
-    // Log end time of the pipeline just for debug
-    var endTime = System.currentTimeMillis()
-    println(s"Training and predicting time: ${(endTime - startTime) / 1000} seconds.")
-
-    // Select only "uid" that is the log's id and the features column containing the Vector predictions
-    // Create new dataframe to not override the original dataset
-    var featured_dataset = predictions_dataset.select("uid", "features")
+  def predictClustering(featured_dataset: DataFrame): DataFrame = {
+    // Log start time just for debug
+    val startTime = System.currentTimeMillis()
 
     // K-means - clustering algorithms that clusters the data points into a predefined number of clusters
     // k-means by default use the Vector features column to predict clusters in a dataframe
     // TODO: Use DBScan algorithm to predict the number of clusters instead of predefined it
-    val kmeans = new KMeans().setK(5)
+    val kmeans = new KMeans()
+      .setK(5)
+      .setPredictionCol("cluster") // To avoid code confusions, rename the "prediction" column added by K-means to "cluster"
 
     // Trains a k-means model.
     val model = kmeans.fit(featured_dataset)
@@ -117,11 +144,21 @@ object AnomalyDetection {
 
     // Predict and clustered the dataframe using the k-means model
     var dataframe_with_clusters = model.transform(featured_dataset)
+
     println("Transform KMeans model:")
     dataframe_with_clusters.show(false)
 
-    // To avoid code confusions, rename the "prediction" column added by K-means to "cluster"
-    dataframe_with_clusters = dataframe_with_clusters.withColumnRenamed("prediction", "cluster")
+    // Log end time of the pipeline just for debug
+    var endTime = System.currentTimeMillis()
+    println(s"Training and predicting Clustering time: ${(endTime - startTime) / 1000} seconds.")
+
+    // Return the dataframe with clustering prediction column
+    dataframe_with_clusters
+  }
+
+  def predictPCA(dataframe_with_clusters: DataFrame): DataFrame = {
+    // Log start time just for debug
+    val startTime = System.currentTimeMillis()
 
     // PCA - convert a set of observations on possibly correlated variables
     // converting features vector into 3-dimensional principal components (x,y,z)
@@ -151,15 +188,10 @@ object AnomalyDetection {
         $"pcaFeaturesArray".getItem(0).as("x"),
         $"pcaFeaturesArray".getItem(1).as("y"))
 
-    // To end join the dataframe with all the anomalous instances
-    // needed for our detection with the original dataset containing the full data log
-    val outlier_dataset = predictions_dataset.join(pca_dataframe, "uid")
+    // Log end time of the pipeline just for debug
+    var endTime = System.currentTimeMillis()
+    println(s"Training and predicting PCA time: ${(endTime - startTime) / 1000} seconds.")
 
-    // Log end time of the full Anomaly Detection prediction, just for debug
-    endTime = System.currentTimeMillis()
-    println(s"Anomaly Detection time: ${(endTime - startTime) / 1000} seconds.")
-
-    // Return original dataset with the new outliers columns "x", "y" and "cluster"
-    outlier_dataset
+    pca_dataframe
   }
 }
