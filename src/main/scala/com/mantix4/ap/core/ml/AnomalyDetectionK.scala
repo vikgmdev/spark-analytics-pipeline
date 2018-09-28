cf9e95bef245280cd87e1571112136e28d7429c5
Victor Garcia <vikgarcia.dev@gmail.com>
2018-09-28 08:14:15 +0800

    Added new logs objects

--- src/main/scala/com/mantix4/ap/core/ml/AnomalyDetectionK.scala
+++ src/main/scala/com/mantix4/ap/core/ml/AnomalyDetectionK.scala
@@ -177,9 +177,11 @@ object AnomalyDetectionK {
     // Predict and clustered the dataframe using the k-means model
     var dataframe_with_clusters = kModel.transform(featured_dataset)
 
-    val distanceFromCenter = udf((features: Vector, c: Int) =>
-      distance(features, kModel.clusterCenters(c))
-    )
+    val distanceFromCenter = udf((features: Vector, c: Int) => {
+      //distance(features, kModel.clusterCenters(c))
+      val b = kModel.clusterCenters(c)
+      math.sqrt(features.toArray.zip(b.toArray).map( p => p._1 - p._2).map(d => d + d).sum)
+    })
 
     dataframe_with_clusters = dataframe_with_clusters.withColumn("distanceFromCenter", distanceFromCenter($"iforestFeatures", $"cluster"))
 
