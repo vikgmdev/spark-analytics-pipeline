a926da1102b26ed357566f1fd483b03bda7cfd97
Victor Garcia <vikgarcia.dev@gmail.com>
2018-10-22 23:25:01 +0800

    Add observations

--- src/main/scala/com/mantix4/ap/core/enrichments/PCROberserver.scala
+++ src/main/scala/com/mantix4/ap/core/enrichments/PCROberserver.scala
@@ -32,8 +32,8 @@ object PCROberserver {
       .withColumn("diff_interval", $"this_time".cast(LongType) - $"last_time".cast(LongType))
 
     df_observed
-      .select("timestamp", "source_ip", "source_port", "dest_ip", "dest_port", "proto", "orig_bytes", "this_time", "last_time", "diff_interval")
-      .show(1000)
+      .select("timestamp", "source_ip", "source_port", "dest_ip", "dest_port", "proto", "orig_bytes", "last_time", "diff_interval")
+      .show(5000)
     df_observed.printSchema()
   }
 
